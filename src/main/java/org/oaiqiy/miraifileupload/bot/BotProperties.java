9db153838e085d9d1328d95014c31988e77d72ee
oaiqiy <1320371940@qq.com>
2021-10-07 15:14:46 +0800

    添加bot属性

--- src/main/java/org/oaiqiy/miraifileupload/bot/BotProperties.java
+++ src/main/java/org/oaiqiy/miraifileupload/bot/BotProperties.java
@@ -1,10 +1,12 @@
 package org.oaiqiy.miraifileupload.bot;
 
 import lombok.Data;
+import org.springframework.boot.context.properties.ConfigurationProperties;
 import org.springframework.stereotype.Component;
 
 @Component
 @Data
+@ConfigurationProperties(prefix = "bot")
 public class BotProperties {
     private Long qqNum;
     private String password;
