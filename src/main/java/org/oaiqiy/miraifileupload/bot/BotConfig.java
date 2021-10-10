e3d43d3633241f1f80d6b94400be8fde2bfb24ec
oaiqiy <1320371940@qq.com>
2021-10-10 16:28:03 +0800

    删除bot事件测试文件

--- src/main/java/org/oaiqiy/miraifileupload/bot/BotConfig.java
+++ src/main/java/org/oaiqiy/miraifileupload/bot/BotConfig.java
@@ -3,7 +3,6 @@ package org.oaiqiy.miraifileupload.bot;
 import lombok.AllArgsConstructor;
 import net.mamoe.mirai.Bot;
 import net.mamoe.mirai.BotFactory;
-import org.oaiqiy.miraifileupload.bot.event.FileEventHandler;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
@@ -12,13 +11,12 @@ import org.springframework.context.annotation.Configuration;
 public class BotConfig {
 
     private BotProperties botProperties;
-    private FileEventHandler fileEvent;
+
 
     @Bean
     public Bot bot(){
         Bot bot = BotFactory.INSTANCE.newBot(botProperties.getQqNum(),botProperties.getPassword());
         bot.login();
-        bot.getEventChannel().registerListenerHost(fileEvent);
         return bot;
 
     }
